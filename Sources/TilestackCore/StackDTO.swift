807d7a1b401c1a0c52324edf30ddd2ba0ee1f74b
Reda Lemeden <git@redalemeden.com>
2024-08-27 01:24:36 +0200

    Add createdAt and updatedAt

--- Sources/TilestackCore/StackDTO.swift
+++ Sources/TilestackCore/StackDTO.swift
@@ -4,10 +4,20 @@ public struct StackDTO: Codable, Hashable, Sendable {
   public var id: UUID
   public var name: String
   public var description: String
+  public var createdAt: Date
+  public var updatedAt: Date?
 
-  public init(id: UUID, name: String, description: String) {
+  public init(
+    id: UUID,
+    name: String,
+    description: String,
+    createdAt: Date = Date(),
+    updatedAt: Date? = nil
+  ) {
     self.id = id
     self.name = name
     self.description = description
+    self.createdAt = createdAt
+    self.updatedAt = updatedAt
   }
 }
