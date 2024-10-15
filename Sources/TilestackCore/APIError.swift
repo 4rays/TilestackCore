04c2113d5571cd414b903cc6341a985584626da3
Reda Lemeden <git@redalemeden.com>
2024-10-15 19:03:59 +0200

    Add empty question/answer error

--- Sources/TilestackCore/APIError.swift
+++ Sources/TilestackCore/APIError.swift
@@ -3,12 +3,14 @@ import Foundation
 public enum APIError: LocalizedError {
   case missingEnvVariable(String)
   case duplicateQuestionAnswer
+  case emptyQuestionOrAnswer
   case entityNotEagerLoaded(String)
 
   public var errorDescription: String? {
     switch self {
     case .missingEnvVariable(let env): "Missing environment variable: \(env)"
     case .duplicateQuestionAnswer: "Question and answer must be unique"
+    case .emptyQuestionOrAnswer: "Question and answer must not be empty"
     case .entityNotEagerLoaded(let entity): "Entity not eager loaded: \(entity)"
     }
   }
