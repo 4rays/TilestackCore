04be5690ceac56602a233b0f52cea42eef2ac86c
Reda Lemeden <git@redalemeden.com>
2024-10-26 22:54:54 +0200

    Add duration to review

--- Sources/TilestackCore/ReviewResultDTO.swift
+++ Sources/TilestackCore/ReviewResultDTO.swift
@@ -3,10 +3,16 @@ import SwiftFSRS
 
 public struct ReviewResultDTO: Codable, Hashable, Sendable {
   public var tileID: UUID
+  public var duration: Double?
   public var review: CardReview
 
-  public init(tileID: UUID, review: CardReview) {
+  public init(
+    tileID: UUID,
+    duration: Double? = nil,
+    review: CardReview
+  ) {
     self.tileID = tileID
+    self.duration = duration
     self.review = review
   }
 }
