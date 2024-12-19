a27d0881e90b0e7573b0beca5b99fcec7e7d4b81
Reda Lemeden <git@redalemeden.com>
2024-12-20 00:53:02 +0100

    Remove session ID from model

--- Sources/TilestackCore/TokenDTO.swift
+++ Sources/TilestackCore/TokenDTO.swift
@@ -3,11 +3,9 @@ import Foundation
 public struct TokenDTO: Codable, Hashable, Sendable {
   public var access: String
   public var refresh: String
-  public var sessionID: String
 
-  public init(access: String, refresh: String, sessionID: String) {
+  public init(access: String, refresh: String) {
     self.access = access
     self.refresh = refresh
-    self.sessionID = sessionID
   }
 }
