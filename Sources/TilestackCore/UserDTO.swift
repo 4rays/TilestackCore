54f7a6a19367ce623b5f19ebe04cc90d4068ea09
Reda Lemeden <git@redalemeden.com>
2025-02-06 00:52:06 +0100

    Add passwordHash to UserDTO

--- Sources/TilestackCore/UserDTO.swift
+++ Sources/TilestackCore/UserDTO.swift
@@ -4,14 +4,17 @@ public struct UserDTO: Codable, Hashable, Sendable {
   public var id: UUID
   public var email: String
   public var name: String?
+  public var passwordHash: String?
 
   public init(
     id: UUID,
     email: String,
-    name: String? = nil
+    name: String? = nil,
+    passwordHash: String? = nil
   ) {
     self.id = id
     self.email = email
     self.name = name
+    self.passwordHash = passwordHash
   }
 }
